f2fa09aa9f8295473ce03fe0259ee60efaf658c4
MKOUHOU <medko24@gmail.com>
2023-10-07 15:23:02 +0100

    Allow CORS

--- AwidServer/Program.cs
+++ AwidServer/Program.cs
@@ -2,7 +2,7 @@ using AwidServer.Contexts;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var clients = "_OriginsClients";
 // Add services to the container.
 
 builder.Services.AddDbContext<AwidContext>(options =>
@@ -12,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: clients,
+        policy =>
+        {
+            policy.WithOrigins("http://127.0.0.1:5173");
+        });
+});
 
 var app = builder.Build();
 
@@ -23,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(clients);
 
 app.UseAuthorization();
 
